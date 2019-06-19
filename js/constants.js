530a59c85bcb04846ccf130c9d297d942bfef13f
Aaron <pengfei.nie@svwtech.com>
2019-06-19 10:54:31 +0800

    merge

---
+++ js/constants.js
@@ -0,0 +1,51 @@
+const THEME_CUS = [
+  {
+    "featureType": "background",
+    "elementType": "all",
+    "stylers": {
+      "color": "#01021fff"
+    }
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.fill",
+    "stylers": {
+      "color": "#2d4170ff"
+    }
+  },
+  {
+    "featureType": "building",
+    "elementType": "all",
+    "stylers": {
+      "color": "#4aa3eaff"
+    }
+  },
+  {
+    "featureType": "all",
+    "elementType": "labels.text.stroke",
+    "stylers": {
+      "color": "#ffffff00"
+    }
+  },
+  {
+    "featureType": "all",
+    "elementType": "labels.text.fill",
+    "stylers": {
+      "color": "#ffffff00"
+    }
+  },
+  {
+    "featureType": "all",
+    "elementType": "labels.icon",
+    "stylers": {
+      "color": "#ffffff00"
+    }
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.stroke",
+    "stylers": {
+      "color": "#2d4c7000"
+    }
+  }
+]
